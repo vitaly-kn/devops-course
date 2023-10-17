2d88de4621e59a67601cdb17ab0148bf95325140
Vitaly Kniazev <vitaly.kniazev@gmail.com>
2023-10-17 23:01:02 +0300

    fix: Improve work with variables

--- module_4/task_1/file-count.sh
+++ module_4/task_1/file-count.sh
@@ -3,5 +3,5 @@ directory="./"
 if [ -n "$1" ]; then
   directory=$1
 fi
-files=`find $directory -type f -name '*' | wc -l`
+files=$(find "$directory" -type f -name '*' | wc -l)
 echo "Directory \"$directory\" contains $files file(s)"
--- module_4/task_1/free-space.sh
+++ module_4/task_1/free-space.sh
@@ -3,8 +3,8 @@ threshold=8000000
 if [ -n "$1" ]; then
   threshold=$1
 fi
-avail=`df --output=avail / | grep '[0-9]'`
+avail=$(df --output=avail / | grep '[0-9]')
 echo "Available disk space is $avail bytes"
-if [ $avail -lt $threshold ]; then
+if [ "$avail" -lt "$threshold" ]; then
   echo "WARNING! Disk free space is below that threshold set for this system ($threshold bytes)"
 fi
